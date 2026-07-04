c8db27e6549fd1cec665ac36671e94554f5f1391
Ali Ahmed <ali.ali.ameen245@gmail.com>
2026-07-04 20:18:06 +0300

    fix: fix auth issue

--- src/app/api/admin/login/route.ts
+++ src/app/api/admin/login/route.ts
@@ -26,7 +26,7 @@ export async function POST(request: Request) {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "lax",
-    path: "/admin",
+    path: "/",
     maxAge: 60 * 60 * 24, // 24 hours
   })
 
